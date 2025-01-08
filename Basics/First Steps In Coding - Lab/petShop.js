40ff70404879c73113fe806b92b9275866ddc265
mmilushev91 <milushev_1990@abv.bg>
2025-01-08 10:07:19 +0200

    Create petShop.js

---
+++ Basics/First Steps In Coding - Lab/petShop.js
@@ -0,0 +1,8 @@
+function solve(dogPackages, catPackages) {
+  const dogPackagePrice = 2.50;
+  const catPackagePrice = 4;
+  
+  let totalPrice = dogPackages * dogPackagePrice + catPackages * catPackagePrice;
+  
+  console.log(`${totalPrice} lv.`)
+}
