dfdeddfc9525035a0af53f368fae120b6231fb5b
mmilushev91 <milushev_1990@abv.bg>
2025-01-08 09:55:24 +0200

    Create projectCreation.js

---
+++ Basics/First Steps In Coding - Lab/projectCreation.js
@@ -0,0 +1,6 @@
+function solve(name, projectCount) {
+  const timePerProject = 3;
+  let timeNeeded = timePerProject * projectCount;
+  
+  console.log(`The architect ${name} will need ${timeNeeded} hours to complete ${projectCount} project/s.`)
+}
