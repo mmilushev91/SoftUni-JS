c199b187c9b931093c1551cc453f40e9f9c54d0a
mmilushev91 <milushev_1990@abv.bg>
2025-01-17 12:07:40 +0200

    Create nxnMatix.js

---
+++ JS-Fundamentals /Functions - Exercise/nxnMatix.js
@@ -0,0 +1,11 @@
+function solve(num) {
+  
+  for (let i = 0; i < num; i++) {
+    const row = [];
+    
+    for (let j = 0; j < num; j++) {
+      row.push(num)
+    }    
+    console.log(row.join(" "))
+  }
+}
