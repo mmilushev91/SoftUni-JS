f16f06bd23096630a125c343002d308c5b1529cc
mmilushev91 <milushev_1990@abv.bg>
2025-01-29 09:41:15 +0200

    Delete JS-Fundamentals /Basic Syntax, Conditional Statements and Loops - More Exercise/echoType.js

--- JS-Fundamentals /Basic Syntax, Conditional Statements and Loops - More Exercise/echoType.js
+++
@@ -1,7 +0,0 @@
-function solve(parameter) {
-  const parameterType = typeof parameter;
-  
-  console.log(parameterType);
-  
-  parameterType === "string" || parameterType === "number" ? console.log(parameter) : console.log("Parameter is not suitable for printing");
-}
