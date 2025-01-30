2f97d80e0a2bfa5b02f372dc72fd87eb57ee900a
mmilushev91 <milushev_1990@abv.bg>
2025-01-30 13:51:30 +0200

    Create centuresToMinutes.js

---
+++ JS-Fundamentals /Data Types and Variables - Lab/centuresToMinutes.js
@@ -0,0 +1,10 @@
+function solve(centuries) {
+  
+  const years = centuries * 100;
+  const days = Math.trunc(years * 365.2422);
+  const hours = days * 24;
+  const minutes = hours * 60;
+  
+  console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`);
+  
+}
