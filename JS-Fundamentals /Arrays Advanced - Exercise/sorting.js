a8d8d0445944763957ea08742b8fb1af822443a9
mmilushev91 <milushev_1990@abv.bg>
2025-02-17 16:34:57 +0200

    Update sorting.js

--- JS-Fundamentals /Arrays Advanced - Exercise/sorting.js
+++ JS-Fundamentals /Arrays Advanced - Exercise/sorting.js
@@ -1,13 +1,22 @@
 function solve(array) {
+  
+  
   const ascending = [...array].sort((a, b) => a - b);
   const descending = [...array].sort((a, b) => b - a);
   
-  const interval = array.length / 2;
+  const arrayLength = array.length;
+  
+  const interval = Math.ceil(arrayLength / 2);
   const output = []
   
   for (let i = 0; i < interval; i++) {
-    output.push(descending[i])
-    output.push(ascending[i])
+    output.push(descending[i]);
+    
+    if (i === interval - 1 && arrayLength % 2 !== 0) {
+      break;
+    }
+
+    output.push(ascending[i]);
   }
   console.log(output.join(" "))
 }
