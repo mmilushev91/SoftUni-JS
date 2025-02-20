fe87cff1efe0bd73a29676c0373fb96985e1fb3b
mmilushev91 <milushev_1990@abv.bg>
2025-02-20 13:57:02 +0200

    Update extractFile.js

--- JS-Fundamentals /Text Processing - Exercise/extractFile.js
+++ JS-Fundamentals /Text Processing - Exercise/extractFile.js
@@ -1,7 +1,11 @@
 function solve(path) {
-  const [name, ...rest] = path.split("\\").slice(-1)[0].split(".");
-  let extension = rest.length > 1 ? rest.slice(-1) : rest[0];
-  
-  console.log(`File name: ${name}`);
+ const pathArray = path.split("\\");
+ const completeFileName = pathArray.slice(-1)[0];
+ const lastPeriodIndex = completeFileName.lastIndexOf(".");
+ 
+ const fileName = completeFileName.slice(0, lastPeriodIndex);
+ const extension = completeFileName.slice(lastPeriodIndex + 1);
+  console.log(`File name: ${fileName}`);
   console.log(`File extension: ${extension}`)
+ 
 }
